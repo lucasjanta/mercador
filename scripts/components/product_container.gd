81d0ca959ebb8853f472e1e3fb2f1e75de3a7f34
lucasjanta <lucastyle2014@gmail.com>
2026-09-16 22:53:31 -0300

    product buttons

---
+++ scripts/components/product_container.gd
@@ -0,0 +1,11 @@
+extends PanelContainer
+@onready var product_name_label: Label = $MarginContainer/HBoxContainer/ProductNameLabel
+@onready var last_update_date_label: Label = $MarginContainer/HBoxContainer/VBoxContainer/LastUpdateDateLabel
+@onready var average_price_label: Label = $MarginContainer/HBoxContainer/AveragePriceLabel
+
+func populate_product(product_id):
+	pass
+
+
+func _on_select_product_button_pressed() -> void:
+	pass # Replace with function body.
